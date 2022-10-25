3ff7fe5eb912caa20aeb0e2ed65a079432af9b2b
Admin <dmtr2001@mail.ru>
2022-10-25 11:26:25 +0700

    новый коммит

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,64 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
+
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+
+        for (int i = 1904; i < 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
+
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
+
+//
+//          int salary = 65535;
+//          int total =0;
+//          for (int i = 0; i <12; i++){
+//              total = total + total/100;
+//              total = total + salary;
+//              System.out.println("Месяц " +i+ " Итого " + total);
+//          }
+//        System.out.println(total);
+//
+//
+//
+//        int total =0;
+//        int cash = 29000;
+//        for (int i = 1; i <=12; i++){
+//            total = total + cash;
+//            System.out.println("Месяц " +i+ " сумма накоплений равна " + total + " рублей.");
+//        }
+//        System.out.println(total);
+//
+//        int total =0;
+//        int cash = 29000;
+//        for (int i = 1; i <=12; i++){
+//            total = total + cash;
+//            total = total + total/100;
+//            System.out.println("Месяц " +i+ " сумма накоплений равна " + total + " рублей.");
+//        }
+//        System.out.println(total);
+//
+//
     }
 }
+
